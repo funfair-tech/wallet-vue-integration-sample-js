c9a553a4774e4f0e65812661e46be328e9fed057
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-01-27 13:29:28 +0000

    change sample code for 3.0.0 wallet release

--- src/services/wallet-service.js
+++ src/services/wallet-service.js
@@ -73,7 +73,7 @@ export function login() {
 }
 
 export function logout() {
-  window.funwallet.sdk.logout();
+  window.funwallet.sdk.auth.logout();
   isAuthenticated$.next(false);
 }
 
@@ -88,7 +88,7 @@ function web3Instance() {
 }
 
 export async function signAMessage(messageText) {
-  const ethereumAddress = await window.funwallet.sdk.ethereumAddress();
+  const ethereumAddress = await window.funwallet.sdk.eth.address();
 
   const result = await web3Instance().eth.personal.sign(
     messageText,
@@ -99,7 +99,8 @@ export async function signAMessage(messageText) {
 }
 
 export async function sendTransaction(tx) {
-  const ethereumAddress = await window.funwallet.sdk.ethereumAddress();
+  const ethereumAddress = await window.funwallet.sdk.eth.address();
+
   tx.from = ethereumAddress;
 
   web3Instance()
@@ -113,5 +114,5 @@ export async function sendTransaction(tx) {
 }
 
 export async function openKycProcess() {
-  await window.funwallet.sdk.kycModalOpen();
+  await window.funwallet.sdk.kyc.start();
 }
